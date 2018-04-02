26829dc88e0103dc9d1ed86556c54fe94fe2fd20
shreyash <shreyash@shreyashs-air>
2018-04-02 18:48:44 +0530

    Initialized Social Network Project

---
+++ web/assets/js/userManage.js
@@ -0,0 +1,34 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(function(){
+    $("#registerForm").submit(function(e){
+        e.preventDefault();
+        $.post("register.jsp",
+            {
+                uname : $("#uname").val(),
+                uemail : $("#uemail").val(),
+                upwd : $("#upwd").val()
+            },
+            function(data,success){
+                alert("User registered Succesfully");
+            });
+        });
+        $("#loginForm").submit(function(e){
+        e.preventDefault();
+        $.post("login.jsp",
+            {
+                uname : $("#uname").val(),
+                uemail : $("#uemail").val(),
+                upwd : $("#upwd").val()
+            },
+            function(data,success){
+                alert("User registered succesfully");
+            });
+        });
+    })
+
+
+
